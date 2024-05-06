d837553d57dee8b4cbe476bdeffd8f790f3452ae
Kevin Zhang <kevinz110608@gmail.com>
2024-05-06 19:50:33 +1200

    Clean database

--- app.py
+++ app.py
@@ -11,8 +11,9 @@ def print_all_aircraft():
     sql = "SELECT * FROM fighter;"
     cursor.execute(sql)
     results = cursor.fetchall()
+    print("name                          speed(km/h)     max_g       climb(m/s)  range(km)       payload(kg)")
     for fighter in results:
-        print(fighter)
+        print(f"{fighter[1]:<30}{fighter[2]:<16}{fighter[3]:<12}{fighter[4]:<12}{fighter[5]:<16}{fighter[6]:<12}")
     db.close()
 
 #main code
