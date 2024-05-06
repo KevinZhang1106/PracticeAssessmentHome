32263f560d0fcd341127a854fda41bd5a4fb4f1f
Kevin Zhang <kevinz110608@gmail.com>
2024-05-06 18:29:28 +1200

    tested database

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import sqlite3
+
+db = sqlite3.connect("fighters.db")
+cursor = db.cursor()
+sql = "SELECT * FROM fighter;"
+cursor.execute(sql)
+results = cursor.fetchall()
+print(results)
+
+db.close()
